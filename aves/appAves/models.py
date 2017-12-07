04f31d7e9f99a9b4a3c8159e47ce62369b47bec8
Andre Herrera <andreherrera97@hotmail.com>
2017-12-07 12:41:23 -0500

    correcion de errores :fire:

--- aves/appAves/models.py
+++ aves/appAves/models.py
@@ -1,9 +1,11 @@
+from __future__ import unicode_literals
 from django.db import models
+
 
 # Create your models here.
 # Tabla aves
 class ave(models.Model):
-	id = models.AutoField(primary_key=True)
+    id = models.AutoField(primary_key=True)
     codigo = models.CharField(max_length=200)
     especie = models.CharField(max_length=200)
     synonim = models.CharField(max_length=0)
@@ -21,20 +23,16 @@ class ave(models.Model):
     id_migracion = models.ForeignKey(migracion)
     id_autor = models.ForeignKey(autor)
     id_bibliografia = models.ForeignKey(bibliografia)
-	def __unicode__(self):
-        return "%s - %s" % (self.id, self.codigo, self.especie, self.synonim, self.nombre, self.morfometres, self.ecology, self.ecology, self.behaviour,
-                           self.yearPublished, self.yearCollection, self.date, self.call, self.observacion, self.id_familia, self.id_endemismo, self.id_migracion,
-                           self.id_autor, self.id_bibliografia)
+    def __unicode__(self):
+        return "%s - %s - %s - %s - %s - %s - %s - %s - %s - %s - %s - %s - %s - %s - %s - %s - %s - %s" % (self.id, self.codigo, self.especie, self.synonim, self.nombre, self.morfometres, self.ecology, self.ecology, self.behaviour, self.yearPublished, self.yearCollection, self.date, self.call, self.observacion, self.id_familia, self.id_endemismo, self.id_migracion, self.id_autor, self.id_bibliografia)
 
 #Familia
 class familia(models.Model):
-	id_familia = models.AutoField(primary_key=True)
-	nombre = models.CharField(max_length=200)
+    id_familia = models.AutoField(primary_key=True)
+    nombre = models.CharField(max_length=200)
     orden = models.CharField(max_length=200)
     def __unicode__(self):
         return "%s - %s - %s" % (self.id_familia, self.nombre, self.orden)
-
-    
 
 #Autor
 class  autor(models.Model):
@@ -52,31 +50,29 @@ class bibliografia(models.Model):
 
 #Migracion
 class migracion(models.Model):
-	id_migracion = models.AutoField(primary_key=True)
-	nombre = models.CharField(max_length=200)
+    id_migracion = models.AutoField(primary_key=True)
+    nombre = models.CharField(max_length=200)
     def __unicode__(self):
         return "%s - %s" % (self.id_migracion, self.nombre)
 
 #UICN
 class uicn(models.Model):
-	id_familia = models.AutoField(primary_key=True)
-	nombre = models.CharField(max_length=200)
+    id_familia = models.AutoField(primary_key=True)
+    nombre = models.CharField(max_length=200)
     def __unicode__(self):
         return "%s - %s" % (self.id_familia, self.nombre)
 
 #Endemismo
 class endemismo(models.Model):
-	id_endemismo = models.AutoField(primary_key=True)
-	nombre = models.CharField(max_length=200)
-     def __unicode__(self):
+    id_endemismo = models.AutoField(primary_key=True)
+    nombre = models.CharField(max_length=200)
+    def __unicode__(self):
         return "%s - %s" % (self.id_endemismo, self.nombre)
 
-
 #Ubicacion
-
 class ubicacion(models.Model):
-	id_ubicacion = models.AutoField(primary_key=True)
-	pais = models.CharField(max_length=200)
+    id_ubicacion = models.AutoField(primary_key=True)
+    pais = models.CharField(max_length=200)
     provincia = models.CharField(max_length=200)
     localidad = models.CharField(max_length=200)
     toponim = models.CharField(max_length=200)
@@ -90,4 +86,3 @@ class ubicacion(models.Model):
         return "%s - %s - %s - %s - %s - %s - %s - %s - %s - %s - %s" % (self.id_ubicacion, self.pais, self.provincia, self.localidad,
                            self.toponim, self.longitud, self.latitud, self.gps, self.latitudMax,
                            self.latitudMin, self.id_ave)
-    
