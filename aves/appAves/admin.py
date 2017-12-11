b8c846b9811da8279637485cb86ed69ebe1c5813
Andre Herrera <andreherrera97@hotmail.com>
2017-12-11 13:28:25 -0500

    Admin Django refinado

--- aves/appAves/admin.py
+++ aves/appAves/admin.py
@@ -1,9 +1,39 @@
 from django.contrib import admin
 from appAves.models import *
 # Register your models here.
+class LocalidadInline(admin.TabularInline):
+    model = Ubicacion
+    extra = 3
 
-admin.site.register(Autores)
-admin.site.register(Ave)
-admin.site.register(Familia)
-admin.site.register(Localidad)
-admin.site.register(Ubicacion)
+
+class UbicacionAdmin(admin.ModelAdmin):
+    inlines = [LocalidadInline]
+    list_display = ('nombre', 'provincia','pais')
+    list_filter = ['provincia']
+    search_fields = ['nombre']
+
+class FamiliasAdmin(admin.ModelAdmin):
+    inlines = []
+    list_display = ('nombre', 'orden')
+    list_filter = ['nombre']
+    search_fields = ['nombre']
+class AvesAdmin(admin.ModelAdmin):
+    inlines = []
+    list_display = ('nombre', 'especie','ecologia')
+    list_filter = ['especie']
+    search_fields = ['nombre']
+class AutoresAdmin(admin.ModelAdmin):
+    inlines = []
+    list_display = ('nombre', 'source')
+    list_filter = ['source']
+    search_fields = ['nombre']
+
+
+
+admin.site.register(Localidad,UbicacionAdmin)
+admin.site.register(Familia,FamiliasAdmin)
+admin.site.register(Autores,AutoresAdmin)
+admin.site.register(Ave,AvesAdmin)
+# admin.site.register(Familia)
+# admin.site.register(Ubicacion)
+# admin.site.register(Localidad)
