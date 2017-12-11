d7fd1354d64b52c77200b923d3ece07ff36b0fea
Andre Herrera <andreherrera97@hotmail.com>
2017-12-11 12:58:59 -0500

    Admin Django

--- aves/appAves/admin.py
+++ aves/appAves/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from appAves.models import *
 # Register your models here.
+
+admin.site.register(Autores)
+admin.site.register(Ave)
+admin.site.register(Familia)
+admin.site.register(Localidad)
+admin.site.register(Ubicacion)
