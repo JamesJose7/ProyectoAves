3d2a6543ec194fad63bde0f474160b58790918b3
Andre Herrera <andreherrera97@hotmail.com>
2017-12-21 11:13:01 -0500

    Creacion de views

--- aves/appAves/views.py
+++ aves/appAves/views.py
@@ -1,3 +1,37 @@
 from django.shortcuts import render
+from django.template import RequestContext
 
-# Create your views here.
+from appAves.models import *
+
+
+def index(request):
+    """
+    """
+    diccionario = {'saludo': "Hola Mundo"}
+    return render(request, 'index.html', diccionario,
+        context_instance=RequestContext(request))
+
+
+def listado_aves(request):
+    """
+        obtengo las aves
+    """
+    aves = Ave.objects.all()
+    diccionario = {'list_aves': aves}
+    return render(request, 'listado_aves.html', diccionario,
+        context_instance=RequestContext(request))
+#
+# def materia(request, id):
+#     """
+#         obtengo las materias
+#     """
+#     materia = Ave.objects.get(pk=id)
+#     numero_paralelos = Ave.objects.filter(la_materia=materia).count()
+#     numero_estudiantes = Estudiante.objects.filter(
+#             paraleloestudiante__la_paralelo__la_materia=materia).count()
+#     diccionario = {'materia': materia, 'numero_paralelos': numero_paralelos,
+#                    'mensaje': 'Mensaje de la pantalla',
+#                    'numero_estudiantes': numero_estudiantes}
+#     return render(request, 'materia.html', diccionario,
+#                   context_instance=RequestContext(request))
+#
