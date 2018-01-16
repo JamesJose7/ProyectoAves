3576a496017865270f174a191a33b72277990984
Andre Herrera <andreherrera97@hotmail.com>
2018-01-16 11:45:58 -0500

    correccion url

--- aves/appAves/urls.py
+++ aves/appAves/urls.py
@@ -4,7 +4,7 @@ from appAves import views
 
 urlpatterns = patterns('',
     url(r'^$', views.index, name='index'),
-    url(r'aves/', views.listado_aves, name='listado_aves'),
+    url(r'listado/', views.listado_aves, name='listado_aves'),
     url(r'ave/(?P<id>\d+)$', views.ave, name='ave'),
     url(r'mapa/', views.mapa_view, name='mapa'),
 
