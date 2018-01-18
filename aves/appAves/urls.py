de8cc5698291ce144e569f93feaf9cd93772ecfb
Andre Herrera <andreherrera97@hotmail.com>
2018-01-17 22:51:46 -0500

    Catalogo de aves, y autores implementado

--- aves/appAves/urls.py
+++ aves/appAves/urls.py
@@ -3,13 +3,12 @@ from django.conf.urls import patterns, url
 from appAves import views
 
 urlpatterns = patterns('',
-    url(r'^$', views.index, name='index'),
-    url(r'listado/', views.listado_aves, name='listado_aves'),
-    url(r'ave/(?P<id>\d+)$', views.ave, name='ave'),
-    url(r'mapa/', views.mapa_view, name='mapa'),
-    url(r'top/', views.top_autores, name='top_autores'),
-    url(r'about/', views.about_view, name='about'),
+                       url(r'^$', views.index, name='index'),
+                       url(r'listado/', views.listado_aves, name='listado_aves'),
+                       url(r'ave/(?P<id>\d+)$', views.ave, name='ave'),
+                       url(r'mapa/', views.mapa_view, name='mapa'),
+                       url(r'top/', views.top_autores, name='top_autores'),
+                       url(r'about/', views.about_view, name='about'),
+                       url(r'autor/(?P<id>\d+)$', views.autor, name='autor'),
 
-
-
-)
+                       )
--- aves/appAves/views.py
+++ aves/appAves/views.py
@@ -9,21 +9,26 @@ def index(request):
     """
     diccionario = {'saludo': "Hola Mundo"}
     return render(request, 'index.html', diccionario,
-        context_instance=RequestContext(request))
+                  context_instance=RequestContext(request))
+
+
 def mapa_view(request):
     lugaresOBJ = lugares.objects.all()
     diccionario = {'lugares': lugaresOBJ}
     return render(request, 'mapa.html', diccionario,
                   context_instance=RequestContext(request))
 
+
 def listado_aves(request):
     """
         obtengo las aves
     """
-    aves = Ave.objects.all()
-    diccionario = {'list_aves': aves}
+    aves = Ave.objects.all().order_by('nombre')[0:50]
+    titulo= "Galeria Aves"
+    diccionario = {'list_aves': aves,'titulo':titulo}
     return render(request, 'listado_aves.html', diccionario,
-        context_instance=RequestContext(request))
+                  context_instance=RequestContext(request))
+
 
 def ave(request, id):
     """
@@ -34,20 +39,36 @@ def ave(request, id):
     diccionario = {'ave': ave}
     return render(request, 'ave.html', diccionario,
                   context_instance=RequestContext(request))
+
+def autor(request, id):
+    """
+        obtengo un autor en especifico
+    """
+    autor = Autores.objects.get(pk=id)
+
+    diccionario = {'autor': autor}
+    return render(request, 'autor.html', diccionario,
+                  context_instance=RequestContext(request))
+
+
+
 def top_autores(request):
     """
     obtengo el top 10 de autores con mas publicaciones
     """
     autores = Autores.objects.all()
-    diccionario = {'lst_autores': autores}
+
+
+    conteo = Ave.objects.all()
+
+    diccionario = {'lst_autores': autores, 'conteo': conteo}
     return render(request, 'top_autores.html', diccionario,
                   context_instance=RequestContext(request))
+
 
 def about_view(request):
     """
     render about
     """
 
-    return render(request, 'about.html',context_instance=RequestContext(request))
-
-
+    return render(request, 'about.html', context_instance=RequestContext(request))
